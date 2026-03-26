109bfa4eaf4e460ff154646444174f7eb69b9511
Yoojin Chung <relish333ever@gmail.com>
2026-03-26 18:23:00 +0900

    Initial Commit

---
+++ CareFlowWatch/CareFlowWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareFlowWatchApp.swift
+//  CareFlowWatch
+//
+//  Created by 정유진 on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CareFlowWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CareFlowWatchTests/CareFlowWatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  CareFlowWatchTests.swift
+//  CareFlowWatchTests
+//
+//  Created by 정유진 on 3/26/26.
+//
+
+import Testing
+@testable import CareFlowWatch
+
+struct CareFlowWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
